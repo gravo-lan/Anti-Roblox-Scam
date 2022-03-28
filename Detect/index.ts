c8af0395a44647b88f5a5c6f8539267113bf9389
GravoLan <66149576+gravo-lan@users.noreply.github.com>
2022-03-28 10:44:43 +0800

    Added blacklist

--- Detect/index.ts
+++ Detect/index.ts
@@ -1,5 +1,19 @@
-const blackList = [];
+const blackList = [
+  "vvvvvv-roblox.com",
+  "www.roblox.qq.com",
+  "www.roblox.cm",
+  "www.roblox.gg",
+  "www.robloxfun.xyz",
+  "www.blox.green",
+  "www.rbxpurple.com",
+  "track.vcdc.com",
+  "www.rovblox.com",
+  "www.roblox.ms"
+];
+
 let url = "www.rovbloxs.com";
+// The current url
+
 if (url.contains("roblox.com") != True) {
   let i = 1;
   do {
@@ -9,7 +23,7 @@ if (url.contains("roblox.com") != True) {
     else {
       i+=1;
     }
-  } while (i<blacklist.length);
+  } while (i<=blacklist.length);
 }
 else {
   let scam = False;
