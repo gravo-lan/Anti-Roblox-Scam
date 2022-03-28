0862aaa557e9397bbe5d5911e9fa0cf32e252ae0
GravoLan <66149576+gravo-lan@users.noreply.github.com>
2022-03-28 17:09:29 +0800

    Added whitelist (incomplete)

--- Detect/index.ts
+++ Detect/index.ts
@@ -1,3 +1,12 @@
+// Define blacklist and whitelist
+const whitelist = [
+  "www.roblox.com", // Will add some code so it doesn't match for roblox.com.mk etc.
+  "www.web.roblox.com",
+  "www.education.roblox.com",
+  "www.ro-blox.com",
+  "www.rowblox.com"
+]
+
 const blackList = [
   "vvvvvv-roblox.com",
   "www.roblox.qq.com",
@@ -12,13 +21,14 @@ const blackList = [
   "www.roblox.com.mk"
 ];
 
+// The current URL
 let url = "www.rovbloxs.com";
-// The current url
 
-if (url.contains("roblox.com") != True) {
+// Check against blacklist
+if (url.contains("roblox.com")) {
   let i = 1;
   do {
-    if (url.contains(blackList[i]) = True) {
+    if (url.contains(blackList[i])) {
       let scam = True;
     }
     else {
@@ -29,4 +39,6 @@ if (url.contains("roblox.com") != True) {
 else {
   let scam = False;
 }
+
+// Todo: Add actions
 console.log(scam);
